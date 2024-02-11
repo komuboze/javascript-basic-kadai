31c35d23fa49e31bb3da108180b6978314ba070f
komuboze <158114956+komuboze@users.noreply.github.com>
2024-02-11 20:57:04 +0900

    課題再提出

---
+++ kadai_020/event.js
@@ -0,0 +1,13 @@
+const addBtn = document.getElementById('btn');
+
+const parentList = document.getElementById('text');
+
+addBtn.addEventListener('click', () => {
+    
+  const childList = document.createElement('text');
+  childList.textContent = 'ボタンをクリックしました';
+
+  parentList.removeChild(parentList.firstChild);
+
+  parentList.appendChild(childList);
+});
